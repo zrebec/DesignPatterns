e124922c785bc1d3af70157e9856ce961a793039
Martin Fox <zrebec@zrebec.sk>
2020-02-02 17:47:21 +0100

    The first concept. I must implement clonable abstract class of Prototype and then clone new instance of primeNumberCalculator and the result +1

---
+++ DesignPatterns/src/sk/zrebec/learn/java/designpatterns/prototype/Prototype.java
@@ -0,0 +1,36 @@
+package sk.zrebec.learn.java.designpatterns.prototype;
+
+import java.util.ArrayList;
+
+public class Prototype {
+	
+	private ArrayList<Integer> primeNumbers = new ArrayList<>();
+	private int maxNumber;
+	
+	public Prototype(int i) {
+		maxNumber = i;
+		this.calculatePrimeNumbers();
+	}
+	
+	private void calculatePrimeNumbers() {
+		
+		for(int i = 2; i <= maxNumber; i++) {
+			boolean prime = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					prime = false;
+					break;
+				}
+			}
+			if (prime) {
+				// This is prime number
+				primeNumbers.add(i);
+			}
+		}
+	}
+
+	public ArrayList<Integer> getPrimeNumbers() {
+		return primeNumbers;
+	}
+
+}
