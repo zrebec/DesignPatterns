7997a82b40a9d78310ed1cde1640b16e05739683
zrebec <zrebec@caroline>
2020-02-12 17:44:20 +0100

    FlyWeight implementation

---
+++ DesignPatterns/src/sk/zrebec/learn/java/designpatterns/flyweight/Program.java
@@ -0,0 +1,132 @@
+package sk.zrebec.learn.java.designpatterns.flyweight;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import java.awt.event.*;
+
+@SuppressWarnings("serial")
+public class Program extends JFrame {
+	
+	
+	/**
+	 * Variable declarations
+	 */
+	JButton startDrawing;
+	int windowWidth = 1750;
+	int windowHeight = 900;
+	Color[] shapeColor = { Color.ORANGE, Color.BLACK, Color.CYAN, Color.RED, Color.GREEN, Color.MAGENTA, Color.YELLOW, Color.PINK, Color.BLUE };
+	private static final long MEGABYTE = 1024L * 1024L;
+
+	public static long bytesToMegabytes(long bytes) {
+		return bytes / MEGABYTE;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		new Program();
+
+	}
+	
+	/**
+	 * Constructor creates a 100000 of rectangles
+	 */
+	public Program() {
+		
+		this.setSize(windowWidth, windowHeight);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("FlyWeight Test");
+		
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new BorderLayout());
+		
+		final JPanel drawingPanel = new JPanel();
+		
+		startDrawing = new JButton("Draw rectangles");
+		
+		contentPane.add(drawingPanel, BorderLayout.CENTER);
+		contentPane.add(startDrawing, BorderLayout.SOUTH);
+		
+		startDrawing.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				Graphics g = drawingPanel.getGraphics();
+				
+				/**
+				 * Start measuring time
+				 */
+				long startTime = System.currentTimeMillis();
+				
+				for (int i = 0; i < 4000000; i++) {
+					
+					/**
+					 * This is classic method when we create a new instance of MyRectangle class
+					 * every single time.
+					 * 
+					 * Please, keep this 2 lines and classic
+					 * constructor commented in MyRectangle class and you will see that FlyWeight should 
+					 * be faster around 200 to 300% on average.
+					 * 
+					 */
+					//MyRectangle rect = new MyRectangle(getRandomColor(), getRandX(), getRandY(), getRandX(), getRandY());
+					//rect.draw(g);
+					
+					/**
+					 * This is FlyWeight method when we create a new instance of rectangle only if
+					 * color is changed. Otherwise just coordinates will be generated. Look at class
+					 * FlyWeightFactory for look how it works.
+					 */
+					MyRectangle rect = RectFactory.getRect(getRandomColor());
+					rect.draw(g, getRandX(), getRandY(), getRandX(), getRandY());
+					
+					
+				}
+				
+				/**
+				 * Calculate memory consumption
+				 */
+				Runtime runtime = Runtime.getRuntime();
+				runtime.gc();
+				
+				long memory = runtime.totalMemory() - runtime.freeMemory();
+				System.out.println("Used memory in byes: " + memory);
+				System.out.println("Used memory in megabyes: " + bytesToMegabytes(memory));
+				
+				/**
+				 * End time measuring and return result to output
+				 */
+				System.out.println("This took " + (System.currentTimeMillis() - startTime) + " miliseconds");
+				
+			}
+		});
+		
+		this.add(contentPane);
+		this.setVisible(true);
+	}
+	
+	/**
+	 * This method returns color or rectangle
+	 * @return Color of rectangle
+	 */
+	private Color getRandomColor()  {
+		Random rn = new Random();
+		return shapeColor[rn.nextInt(shapeColor.length - 1)];
+	}
+	
+	/**
+	 * Method returns random X position
+	 * @return Integer of X
+	 */
+	private int getRandX() {return (int)(Math.random()*windowWidth);}
+	
+	/**
+	 * Method returns random Y position
+	 * @return Integer of Y
+	 */
+	private int getRandY() {return (int)(Math.random()*windowHeight);}
+
+}
---
+++ DesignPatterns/src/sk/zrebec/learn/java/designpatterns/flyweight/RectFactory.java
@@ -0,0 +1,50 @@
+package sk.zrebec.learn.java.designpatterns.flyweight;
+
+import java.util.HashMap;
+import java.awt.Color;
+
+/**
+ * This class is a sample of FlyWeight implementation.
+ * 
+ * FlyWeight Factory is a class which is useful if we
+ * have many objects with similar properties. Primary 
+ * goal minimize memory usage if object already exists 
+ * recycle it and we just create a new one if similar object
+ * doesn't exists yet in pool.
+ * 
+ * We have Intrinsic properties: Which are same for Object
+ * and Extrinsic which are different for Object.
+ * 
+ * This can also speed up the application but this not primary
+ * goal of FlyWeight design pattern.
+ * 
+ * In this case, we have HashMap with rectangles by color.
+ * If we doesn't have color as a key in HashMap, we create a new
+ * instance of rectangle. Otherwise we get existing rectangle 
+ * from HashMap and return it. Then only a dimensions of rectangle
+ * will be created in draw method.
+ * 
+ * @author friskyfox
+ * @version 1.0
+ *
+ */
+
+public class RectFactory {
+
+	private static final HashMap<Color, MyRectangle> rectsByColor = new HashMap<Color, MyRectangle>();
+	
+	public static MyRectangle getRect(Color color) {
+		
+		MyRectangle rect = (MyRectangle)rectsByColor.get(color);
+		
+		if (rect == null) { 
+			
+			rect = new MyRectangle(color);
+			rectsByColor.put(color, rect);
+		}
+		
+		return rect;
+		
+	}
+	
+}
