7c3cb5de4abf4911801e4b0ce3fdbb18fd8afd24
zrebec <zrebec@zrebec.sk>
2023-05-03 22:46:26 +0200

    fix: grammar changes

--- sk/zrebec/learn/java/designpatterns/factory/Program.java
+++ sk/zrebec/learn/java/designpatterns/factory/Program.java
@@ -11,12 +11,12 @@ import java.util.Scanner;
  * The good example is a generating enemies in game engine
  * Imagine, that you have more enemy types (zombies, troopers or robots).
  * Every enemy has a different speed, strength, stamina and damage.
- * But every enemy has all this parameters, only with different implementation. 
+ * But every enemy has all these parameters, only with different implementation.
  * 
  * See Program.java for implementation.
  *  
  * @author friskyfox
- * @version 1.0
+ * @version 1.0.1
  *
  */
 
@@ -45,16 +45,16 @@ public class Program {
 		
 		/*
 		  How to balance game?
-		  Every enemy attacks to the hero. Speed of enemies are different but we doesn't make the difference
-		  when is is the hero faster. Hero can run in one move random distance from 1 to 100m.
+		  Every enemy attacks to the hero. Speed of enemies are different, but we don't make the difference
+		  when is the hero faster. Hero can run in one move random distance from 1 to 100m.
 
-		   We must implement this to the enemies. Enemies has speed from 1 to 20. 1 represents 5 meters (20*5 = 100)
+		   We must implement this to the enemies. Enemy has speed from 1 to 20. 1 represents 5 meters (20*5 = 100)
 
-		   If hero will get higher number before attack, he ran away and enemy cannot attack
+		   If hero get higher number before attack, he ran away and enemy cannot attack
 
-		   Of course, we must find the balance. That means Fast Zombie will must to have very high chance to catch the
-		   hero and attacks. Normal Zombie will must have lowest chance. But Fast Zombie gets a small attack and but normal
-		   zombie makes the attack with highest damage. Trooper and Robots must be re-balanced too.
+		   Of course, we must find the balance. That means Fast Zombie will must have very high chance to catch the
+		   hero and attacks. Normal Zombie will must have the lowest chance. But Fast Zombie gets a small attack and normal
+		   zombie makes the attack with the highest damage. Trooper and Robots must be re-balanced too.
 
 		   Don't remember, hero have a small health regeneration after every move.
 
