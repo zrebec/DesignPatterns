b3b198aea5f124ebc7b2edc5f91e2007299f26e0
zrebec <zrebec@zrebec.sk>
2023-05-06 17:50:55 +0200

    feat: new switch for jdk 14 and later

--- sk/zrebec/learn/java/designpatterns/factory/EnemyFactory.java
+++ sk/zrebec/learn/java/designpatterns/factory/EnemyFactory.java
@@ -3,20 +3,13 @@ package sk.zrebec.learn.java.designpatterns.factory;
 public class EnemyFactory {
 	
 	public Enemy makeEnemy(String enemyOption) {
-		
-		switch (enemyOption.toUpperCase()) {
-		case "T":
-			return new Trooper();
-		case "R":
-			return new Robot();
-		case "Z":
-			return new Zombie();
-		case "F":
-			return new FastZombie();
-		default:
-			return null;
-		}
-		
+		return switch (enemyOption.toUpperCase()) {
+			case "T" -> new Trooper();
+			case "R" -> new Robot();
+			case "Z" -> new Zombie();
+			case "F" -> new FastZombie();
+			default -> null;
+		};
 	}
 
 }
