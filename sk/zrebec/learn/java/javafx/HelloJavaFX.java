b396b2b481f8d913fe1727897873c877aae5b84c
zrebec <zrebec@zrebec.sk>
2023-05-06 15:58:54 +0200

    feat: dynamic font size

--- sk/zrebec/learn/java/javafx/HelloJavaFX.java
+++ sk/zrebec/learn/java/javafx/HelloJavaFX.java
@@ -3,9 +3,11 @@ package sk.zrebec.learn.java.javafx;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -19,19 +21,33 @@ public class HelloJavaFX extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        Label label = new Label();
-        label.setTextFill(Color.GREEN); // Set text color to green
-        label.setText("Hello, Java!");
-        label.setFont(new Font(24)); // Set font size
+        // Label for Welcome message
+        Label labelWelcomeMessage = new Label();
+        labelWelcomeMessage.setTextFill(Color.GREEN); // Set text color to green
+        labelWelcomeMessage.setText("Hello, Java!");
 
-        StackPane root = new StackPane(label);
+        // Label for actual datetime
+        Label labelTimestamp = new Label();
+        labelTimestamp.setTextFill(Color.YELLOW); // Set text color to green
+        labelTimestamp.setText("Hello, Java!");
+
+        VBox root = new VBox(10); // Create VBox with 10px spacing between elements
+        root.getChildren().addAll(labelWelcomeMessage, labelTimestamp);
         root.setStyle("-fx-background-color: black"); // Set the root pane to black background
+        root.setAlignment(Pos.CENTER); // Center the elements inside the VBox
         Scene scene = new Scene(root, 600, 200);
 
+        // Bind font size to the minimum of scene width and height divided by a scaling factor
+        double scalingFactor = 10.0;
 
-        primaryStage.setTitle("Hello JavaFX");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        // Example for using lambda functions
+        labelWelcomeMessage.fontProperty().bind(Bindings.createObjectBinding(
+            () -> new Font(Math.min(scene.getWidth(), scene.getHeight()) / scalingFactor),
+            scene.widthProperty(), scene.heightProperty()));
+
+        labelTimestamp.fontProperty().bind(Bindings.createObjectBinding(
+            () -> new Font(Math.min(scene.getWidth(), scene.getHeight()) / (scalingFactor * 1.5)),
+            scene.widthProperty(), scene.heightProperty()));
 
         // Set Date and Time
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
@@ -40,12 +56,17 @@ public class HelloJavaFX extends Application {
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
             LocalTime currentTime = LocalTime.now();
             String formattedTime = currentTime.format(formatter);
-            label.setText("Hello, JavaFX! The current time is: " + formattedTime);
+            labelTimestamp.setText("Hello, JavaFX! The current time is: " + formattedTime);
         }));
 
         // Set cyclic timeline
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
+
+        // Show primary stage
+        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
